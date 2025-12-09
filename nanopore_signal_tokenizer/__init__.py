fa5ce16cd507b42c9acee432a583666409ff5130
jiaoshuai <jiaoshuaihit@gmail.com>
2025-12-09 17:22:14 +0800

    更新init

--- nanopore_signal_tokenizer/__init__.py
+++ nanopore_signal_tokenizer/__init__.py
@@ -4,6 +4,7 @@ from .nanopore import nanopore_normalize
 from .nanopore import nanopore_filter
 from .fast5 import Fast5Dir
 from .rvq_tokenizer import RVQTokenizer
+from .kmeans_tokenizer import KmeansTokenizer
 # 或者更精细地控制导出内容，避免 * 导入
 __version__ = "0.1.0"
-__all__ = ["RVQTokenizer", "Fast5Dir"]
+__all__ = ["RVQTokenizer", "Fast5Dir","KmeansTokenizer"]
