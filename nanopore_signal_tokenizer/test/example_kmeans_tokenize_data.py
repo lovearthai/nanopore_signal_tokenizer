7ef914f7519e3f9546c804349ad288cc83866f6f
root <lovearthai@gmail.com>
2026-01-14 16:36:25 +0800

    更新获取window_size 和 stride

--- nanopore_signal_tokenizer/test/example_kmeans_tokenize_data.py
+++ nanopore_signal_tokenizer/test/example_kmeans_tokenize_data.py
@@ -4,7 +4,8 @@ from nanopore_signal_tokenizer.kmeans_tokenizer import KmeansTokenizer
 tokenizer = KmeansTokenizer(
     centroids_path="../models/centroids_with_meta.npy",
 )
-
+window_size = tokenizer.window_size
+stride = tokenizer.stride
 # 模拟一段 1200 点的信号（~240ms @ 5kHz）
 signal = np.random.randn(1200).astype(np.float32) * 5 + 100
 
